cfacce4edde741323793e5f904347f22fb821019
Marko Grešak <marko@gresak.io>
2021-03-02 17:08:13 +0100

    Remove unused import

--- src/components/OrderBook/OrderBook.tsx
+++ src/components/OrderBook/OrderBook.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import styled from '@emotion/styled';
 import useSubscribeToOrderBook from '../../api/useSubscribeToOrderBook';
 import productIdToName from '../../lib/productIdToName';
